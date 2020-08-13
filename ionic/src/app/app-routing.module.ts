8d0e8960dfcd12f7f8eaff08bf67fac16bfc9d14
dr.zivkovic <dr.zivkovic@gmail.com>
2020-08-13 13:40:34 +0200

    delete some commented code

--- ionic/src/app/app-routing.module.ts
+++ ionic/src/app/app-routing.module.ts
@@ -15,23 +15,3 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-
-// const routes: Routes = [
-//   {
-//     path: 'home',
-//     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-//   },
-//   {
-//     path: '',
-//     redirectTo: 'home',
-//     pathMatch: 'full'
-//   },
-// ];
-
-// @NgModule({
-//   imports: [
-//     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-//   ],
-//   exports: [RouterModule]
-// })
-// export class AppRoutingModule { }
